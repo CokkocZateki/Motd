4658eed949e8552130c1bc51c203c8732be84e1d
CokkocZateki <tenayamasai2@gmail.com>
2025-07-13 11:07:12 +0200

    Delete madashboard directory

--- madashboard/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class MadashboardConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "madashboard"
-    verbose_name = "MOTD Dashboard"
