53298b8f48dcd379e493a1fc7d8587daa11df7aa
CokkocZateki <tenayamasai2@gmail.com>
2025-08-14 02:47:55 +0200

    Update 0001_initial.py

--- motd/migrations/0001_initial.py
+++ motd/migrations/0001_initial.py
@@ -19,59 +19,41 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(help_text='Title of the MOTD message', max_length=200)),
                 ('content', models.TextField(help_text='Message content (HTML allowed)')),
-                (
-                    'style',
-                    models.CharField(
-                        choices=[
-                            ('info', 'Info (Blue)'),
-                            ('success', 'Success (Green)'),
-                            ('warning', 'Warning (Yellow)'),
-                            ('danger', 'Danger (Red)')
-                        ],
-                        default='info',
-                        help_text='Bootstrap alert style',
-                        max_length=10,
-                    ),
-                ),
+                ('style', models.CharField(
+                    choices=[
+                        ('info', 'Info (Blue)'),
+                        ('success', 'Success (Green)'),
+                        ('warning', 'Warning (Yellow)'),
+                        ('danger', 'Danger (Red)')
+                    ],
+                    default='info',
+                    help_text='Bootstrap alert style',
+                    max_length=10,
+                )),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('updated_at', models.DateTimeField(auto_now=True)),
-                (
-                    'start_date',
-                    models.DateTimeField(
-                        default=django.utils.timezone.now,
-                        help_text='When this message should start displaying',
-                    ),
-                ),
-                (
-                    'end_date',
-                    models.DateTimeField(
-                        blank=True,
-                        help_text='When this message should stop displaying (leave blank for permanent)',
-                        null=True,
-                    ),
-                ),
+                ('start_date', models.DateTimeField(
+                    default=django.utils.timezone.now,
+                    help_text='When this message should start displaying',
+                )),
+                ('end_date', models.DateTimeField(
+                    blank=True,
+                    help_text='When this message should stop displaying (leave blank for permanent)',
+                    null=True,
+                )),
                 ('is_active', models.BooleanField(default=True, help_text='Whether this message is active')),
-                (
-                    'show_to_all',
-                    models.BooleanField(default=True, help_text='Show to all authenticated users'),
-                ),
-                (
-                    'created_by',
-                    models.ForeignKey(
-                        null=True,
-                        on_delete=django.db.models.deletion.SET_NULL,
-                        related_name='created_motd_messages',
-                        to=settings.AUTH_USER_MODEL,
-                    ),
-                ),
-                (
-                    'restricted_to_groups',
-                    models.ManyToManyField(
-                        blank=True,
-                        help_text='If specified, only show to users in these groups',
-                        to='auth.group',
-                    ),
-                ),
+                ('show_to_all', models.BooleanField(default=True, help_text='Show to all authenticated users')),
+                ('created_by', models.ForeignKey(
+                    null=True,
+                    on_delete=django.db.models.deletion.SET_NULL,
+                    related_name='created_motd_messages',
+                    to=settings.AUTH_USER_MODEL,
+                )),
+                ('restricted_to_groups', models.ManyToManyField(
+                    blank=True,
+                    help_text='If specified, only show to users in these groups',
+                    to='auth.group',
+                )),
             ],
             options={
                 'verbose_name': 'MOTD Message',
@@ -108,4 +90,3 @@ class Migration(migrations.Migration):
             },
         ),
     ]
-
