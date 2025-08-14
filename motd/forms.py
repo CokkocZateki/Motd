9c6168f926e0836f99ead83ef0b8426f03cbc376
CokkocZateki <tenayamasai2@gmail.com>
2025-08-15 00:15:44 +0200

    Update forms.py

--- motd/forms.py
+++ motd/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-
 from .models import MotdMessage
 
 
@@ -17,8 +16,18 @@ class MotdMessageForm(forms.ModelForm):
             "restricted_to_groups",
         ]
         widgets = {
-            "start_date": forms.DateTimeInput(attrs={"type": "datetime-local"}),
-            "end_date": forms.DateTimeInput(attrs={"type": "datetime-local"}),
-            "restricted_to_groups": forms.CheckboxSelectMultiple,
+            "content": forms.Textarea(attrs={"rows": 5, "class": "form-control"}),
+            "start_date": forms.DateTimeInput(attrs={"type": "datetime-local", "class": "form-control"}),
+            "end_date": forms.DateTimeInput(attrs={"type": "datetime-local", "class": "form-control"}),
+            "restricted_to_groups": forms.SelectMultiple(attrs={"class": "form-control select2", "size": "10"}),
+            "style": forms.Select(attrs={"class": "form-control"}),
+            "title": forms.TextInput(attrs={"class": "form-control"}),
         }
-
+        labels = {
+            "style": "Priority Level",
+            "show_to_all": "Show to all Members",
+        }
+        help_texts = {
+            "show_to_all": "Show to all users with Member state",
+            "restricted_to_groups": "Select specific groups (use Ctrl/Cmd to select multiple)",
+        }
