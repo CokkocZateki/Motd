6d12ecfc43fc5f892f9fa1653525bef8378d2139
CokkocZateki <tenayamasai2@gmail.com>
2025-08-14 02:26:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aa-motd',
-    version='1.0.26',
+    version='1.0.27',
     author='Cokkoc Zateki',
     author_email='N/A',
     description='Message of the Day app for Alliance Auth',
